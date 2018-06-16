aad8fdb4186435cb80586ac6fb5b7ae4070e9182
shirasu <shira7867@gmail.com>
2018-06-16 19:54:53 +0900

    add

---
+++ ProjectIDEA-Model/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ProjectIDEAModel
+{
+    public class Item
+    {
+        public string UserName { get; set; }
+        public int Id { get; set; }
+        public string SongName { get; set; }
+        public string ArtistName { get; set; }
+        public string ArtWork { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ ProjectIDEA-Model/Song.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProjectIDEAModel
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string SongName { get; set; }
+        public string ArtistName { get; set; }
+        public string ArtWork { get; set; }
+    }
+}
