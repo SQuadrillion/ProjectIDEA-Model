899403d10e9cbedb19b72acee71db9fd33704289
shirasu <shira7867@gmail.com>
2018-06-16 19:47:44 +0900

    [rename & modify] ItemObject & SongObject

---
+++ ProjectIDEA-Model/ItemObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ProjectIDEAModel
+{
+    [JsonObject]
+    public class ItemObject
+    {
+        [JsonProperty("id")]         public int Id { get; private set; }
+
+        [JsonProperty("song")]
+        public SongObject SongObject { get; private set; }
+
+        [JsonProperty("username")]
+        public string UserName { get; private set; }
+
+        public ItemObject(int id, SongObject songObject, string userName)
+        {
+            this.Id = id;
+            this.SongObject = songObject;
+            this.UserName = userName;
+        }
+    }
+}
---
+++ ProjectIDEA-Model/SongObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ProjectIDEAModel
+{
+    [JsonObject]
+    public class SongObject
+    {
+        [JsonProperty("id")]
+        public int Id { get; private set; }
+
+        [JsonProperty("song_name")]
+        public string SongName { get; private set; }
+
+        [JsonProperty("artist_name")]
+        public string ArtistName { get; private set; }
+
+        [JsonProperty("artwork_url")]
+        public string ArtworkUrl { get; private set; }
+
+        [JsonProperty("time")]
+        public string Time { get; private set; }
+
+        [JsonProperty("music_url")]
+        public string MusicUrl { get; private set; }
+
+        public SongObject(int id, string songName, string artistName, string artworkUrl, string time, string musicUrl)
+        {
+            this.Id = id;
+            this.SongName = songName;
+            this.ArtistName = artistName;
+            this.ArtworkUrl = artworkUrl;
+            this.Time = time;
+            this.MusicUrl = musicUrl;
+        }
+    }
+}
