9f087297951f9fd792f145514d320be95dbdfac1
shirasu <shira7867@gmail.com>
2018-06-17 02:11:54 +0900

    fix

--- ProjectIDEA-Model/Program.cs
+++ ProjectIDEA-Model/Program.cs
@@ -8,7 +8,20 @@ namespace ProjectIDEAModel
         static void Main(string[] args)
         {
             IdeaRemote ideaRemote = new IdeaRemote();
-            Console.WriteLine(ideaRemote.PostItemAsync(1, "shirauu").Result);
+            Console.WriteLine(ideaRemote.PostItemAsync(1, "shirasu").Result);
+
+            foreach(var song in ideaRemote.GetSongsAsync().Result)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", song.Id, song.ArtistName, song.ArtWork, song.SongName, song.Url);
+            }
+
+            foreach (var item in ideaRemote.GetItemsAsync().Result)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", item.Id, item.ArtistName, item.ArtWork, item.SongName, item.Url, item.UserName);
+            }
+
+            var playing = ideaRemote.GetPlayingAsync().Result;
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", playing.Id, playing.ArtistName, playing.ArtWork, playing.SongName, playing.Url, playing.UserName);
         }
     }
 }
